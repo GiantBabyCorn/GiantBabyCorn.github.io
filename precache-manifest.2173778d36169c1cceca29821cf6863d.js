ca6f8ca58f6f5d191341d16203e3099dd345439d
giantbabycorn <82532128+giantbabycorn@users.noreply.github.com>
2021-04-15 11:19:04 +0800

    init

---
+++ precache-manifest.2173778d36169c1cceca29821cf6863d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe766c380bed6fe164b7a4b0d2e9178",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caf36fef8b58b3f2ee0b",
+    "url": "/static/css/main.bb3bac4d.chunk.css"
+  },
+  {
+    "revision": "7e4b473a199ca26dfe43",
+    "url": "/static/js/2.9cd6bd08.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.9cd6bd08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caf36fef8b58b3f2ee0b",
+    "url": "/static/js/main.ce20cf77.chunk.js"
+  },
+  {
+    "revision": "9fc1b2ebbbabe255ca0e",
+    "url": "/static/js/runtime-main.9a566bfa.js"
+  },
+  {
+    "revision": "812c50feba26633cc29d5187b2305dc5",
+    "url": "/static/media/icon-fail.812c50fe.svg"
+  },
+  {
+    "revision": "c04c757fa6658d5bacc0870ce2a794fe",
+    "url": "/static/media/icon-success.c04c757f.svg"
+  }
+]);
